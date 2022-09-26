eafb4ef9016978b47abfed40d23211976b9bed3d
Hoang Minh <97202270+Cyclops125@users.noreply.github.com>
2022-09-26 12:39:40 +0700

    finish ping

--- PingReso/Program.cs
+++ PingReso/Program.cs
@@ -12,92 +12,113 @@ using System.Diagnostics;
 
 
 namespace PingReso
-{           
+{
     class Program
     {
+
+        public class Domain
+        {
+            public string Domains { get; set; }
+        }
+       
+
         static void Main(string[] args)
         {
-            
+
             Ping p = new Ping();
             bool check = true;
-            
+
             //Read Json file
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory
                 + @"appsettings.json");
 
             using (StreamReader sr = new StreamReader(path))
             {
-                var json = sr.ReadToEnd();
-                var list = JsonConvert.DeserializeObject<List<string>>(json);
-                //var hashmap = new Dictionary<string, int>();
-                //var hashmap = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
-                var res = list.Distinct().ToDictionary(x => x, x => x);
-                //foreach (var item in list)
-                //{
-                //    hashmap.Add(item, 0);
-                //}
 
-                for (int j = 0; j< 2; j++ )
+                var json = sr.ReadToEnd();
+                var list = JsonConvert.DeserializeObject<Domain>(json);
+
+                string[] convertList = list.Domains.Split(",");
+                List<string> newList = new List<string>(convertList);
+
+
+                Dictionary<string, int> hashmap = newList.Distinct().ToDictionary(x => x, x => 0);
+                for (; ; )
                 {
-                    foreach (var item in res)
+                    foreach (var item in hashmap)
                     {
                         //Console.WriteLine(list);
                         while (check)
                         {
-                            try
+                            if (hashmap[item.Key] < 2)
                             {
-                                PingReply rep = p.Send(res.ToString(), 1000);
-                                if (rep.Status.ToString() == "Success")
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Cyan;
 
-                                    Console.WriteLine("Reply from: " + rep.Address + "Bytes=" + rep.Buffer.Length + " Time=" +
-                                        rep.RoundtripTime + " TTL=" + rep.Options.Ttl + " Routers=" + (128 - rep.Options.Ttl) + " Status=" +
-                                        rep.Status + " Server" + res);
-                                    Thread.Sleep(1000);
+
+                                try
+                                {
+                                    PingReply rep = p.Send(item.Key, 1000);
+                                    if (rep.Status.ToString() == "Success")
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Cyan;
+
+                                        Console.WriteLine("Reply from: " + rep.Address + "Bytes=" + rep.Buffer.Length + " Time=" +
+                                            rep.RoundtripTime + " TTL=" + rep.Options.Ttl + " Routers=" + (128 - rep.Options.Ttl) + " Status=" +
+                                            rep.Status + " Server" + item);
+                                        Thread.Sleep(1000);
+
+                                    }
+
+                                    check = false;
+
 
                                 }
-                                else
+                                catch (Exception ex)
                                 {
                                     Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine("Request Timed Out");
-                                    Thread.Sleep(100);
+
+                                    hashmap[item.Key] = item.Value + 1;
+                                    Console.WriteLine("Error:{0}, {1}", item.Key, hashmap[item.Key]);
+                                    if (hashmap[item.Key] == 2)
+                                    {
+
+                                        Console.WriteLine("Error at server: {0}",  item.Key);
+                                        
+
+
+                                        string logFolderName = "logs/";
+                                        if (!Directory.Exists(logFolderName))
+                                        {
+                                            Directory.CreateDirectory(logFolderName);
+                                        }
+                                        string logFileName = "";
+                                        DateTime now = DateTime.Now;
+                                        logFileName = String.Format("{0}_{1}_{2}_log.txt", now.Day, now.Month, now.Year);
+                                        string fullFileLog = Path.Combine(logFolderName, logFileName);
+
+                                        using (StreamWriter sw = new StreamWriter(fullFileLog))
+                                        {
+                                            sw.WriteLine(String.Format("Error occurs at: {0}", now));
+                                            sw.WriteLine(String.Format("Error: {0}", ex.Message));
+                                        }
+
+                                    }
+
+
+
+                                    Thread.Sleep(1500);
+
+
+                                    check = false;
+
+
                                 }
-
-                            check = false;
-                                
-
                             }
-                            catch (Exception ex)
+                            else
                             {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Server do not exist!!!!");
-                               
-                               
-
-                                //string logFolderName = "logs/";
-                                //if (!Directory.Exists(logFolderName))
-                                //{
-                                //    Directory.CreateDirectory(logFolderName);
-                                //}
-                                //string logFileName = "";
-                                //DateTime now = DateTime.Now;
-                                //logFileName = String.Format("{0}_{1}_{2}_log.txt", now.Day, now.Month, now.Year);
-                                //string fullFileLog = Path.Combine(logFolderName, logFileName);
-
-                                //using (StreamWriter sw = new StreamWriter(fullFileLog))
-                                //{
-                                //    sw.WriteLine(String.Format("Error occurs at: {0}", now));
-                                //    sw.WriteLine(String.Format("Error: {0}", ex.Message));
-                                //}
-                                Thread.Sleep(1500);
-                                //lưu vô 1 list
-                      
                                 check = false;
-                                
-                        
                             }
                         }
+
                         check = true;
                     }
 
@@ -108,4 +129,6 @@ namespace PingReso
         }
     }
 }
+
+
 
