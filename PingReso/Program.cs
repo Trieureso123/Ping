ca143211334bdd1ef2510cfb03ed7988d3c48e5f
HTrieuhehe <trieu.huynh@reso.vn>
2022-09-26 16:13:22 +0700

    update

--- PingReso/Program.cs
+++ PingReso/Program.cs
@@ -9,7 +9,10 @@ using System.Linq;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Diagnostics;
-
+using System.Net.Mail;
+using System.Reflection.Metadata;
+using Microsoft.VisualBasic;
+using System.Net.Http;
 
 namespace PingReso
 {
@@ -24,108 +27,129 @@ namespace PingReso
 
         static void Main(string[] args)
         {
+            string email = "Trieuhchse161563@fpt.edu.vn";
+            string body = "bla bla bla";
 
-            Ping p = new Ping();
-            bool check = true;
+            sendEmail(email, body);
 
-            //Read Json file
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory
-                + @"appsettings.json");
+            //Ping p = new Ping();
+            //bool check = true;
 
-            using (StreamReader sr = new StreamReader(path))
+            ////Read Json file
+            //string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory
+            //    + @"appsettings.json");
+
+            //using (StreamReader sr = new StreamReader(path))
+            //{
+
+            //    var json = sr.ReadToEnd();
+            //    var list = JsonConvert.DeserializeObject<Domain>(json);
+
+            //    string[] convertList = list.Domains.Split(",");
+            //    List<string> newList = new List<string>(convertList);
+
+
+            //    Dictionary<string, int> hashmap = newList.Distinct().ToDictionary(x => x, x => 0);
+            //    for (; ; )
+            //    {
+            //        foreach (var item in hashmap)
+            //        {
+            //            //Console.WriteLine(list);
+            //            while (check)
+            //            {
+            //                if (hashmap[item.Key] < 2)
+            //                {
+            //                    try
+            //                    {
+            //                        PingReply rep = p.Send(item.Key, 1000);
+            //                        if (rep.Status.ToString() == "Success")
+            //                        {
+            //                            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            //                            Console.WriteLine("Reply from: " + rep.Address + "Bytes=" + rep.Buffer.Length + " Time=" +
+            //                                rep.RoundtripTime + " TTL=" + rep.Options.Ttl + " Routers=" + (128 - rep.Options.Ttl) + " Status=" +
+            //                                rep.Status + " Server" + item);
+            //                            Thread.Sleep(1000);
+            //                        }
+            //                        check = false;
+            //                    }
+            //                    catch (Exception ex)
+            //                    {
+            //                        Console.ForegroundColor = ConsoleColor.Red;
+
+            //                        hashmap[item.Key] = item.Value + 1;
+            //                        Console.WriteLine("Error:{0}, {1}", item.Key, hashmap[item.Key]);
+            //                        if (hashmap[item.Key] == 2)
+            //                        {
+
+            //                            Console.WriteLine("Error at server: {0}",  item.Key);
+
+
+
+            //                            string logFolderName = "logs/";
+            //                            if (!Directory.Exists(logFolderName))
+            //                            {
+            //                                Directory.CreateDirectory(logFolderName);
+            //                            }
+            //                            string logFileName = "";
+            //                            DateTime now = DateTime.Now;
+            //                            logFileName = String.Format("{0}_{1}_{2}_log.txt", now.Day, now.Month, now.Year);
+            //                            string fullFileLog = Path.Combine(logFolderName, logFileName);
+
+            //                            using (StreamWriter sw = new StreamWriter(fullFileLog))
+            //                            {
+            //                                sw.WriteLine(String.Format("Error occurs at: {0}", now));
+            //                                sw.WriteLine(String.Format("Error: {0}", ex.Message));
+            //                            }
+
+            //                        }
+            //                        Thread.Sleep(1500);
+            //                        check = false;
+            //                    }
+            //                }
+            //                else
+            //                {
+            //                    check = false;
+            //                }
+            //            }
+
+            //            check = true;
+            //        }
+
+            //    }
+
+            //}
+
+        }
+
+
+        public static void sendEmail(string email, string body)
+        {
+            if (String.IsNullOrEmpty(email))
+                return;
+            try
             {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(email);
+                mail.From = new MailAddress("Trieuhchse161563@fpt.edu.vn");
+                mail.Subject = "sub";
 
-                var json = sr.ReadToEnd();
-                var list = JsonConvert.DeserializeObject<Domain>(json);
+                mail.Body = body;
 
-                string[] convertList = list.Domains.Split(",");
-                List<string> newList = new List<string>(convertList);
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com"; //Or Your SMTP Server Address
+                smtp.Credentials = new System.Net.NetworkCredential("Trieuhchse161563@gfpt.edu.vn", "0775711152haitrieu"); // ***use valid credentials***
+                smtp.Port = 587;
 
-
-                Dictionary<string, int> hashmap = newList.Distinct().ToDictionary(x => x, x => 0);
-                for (; ; )
-                {
-                    foreach (var item in hashmap)
-                    {
-                        //Console.WriteLine(list);
-                        while (check)
-                        {
-                            if (hashmap[item.Key] < 2)
-                            {
-
-
-                                try
-                                {
-                                    PingReply rep = p.Send(item.Key, 1000);
-                                    if (rep.Status.ToString() == "Success")
-                                    {
-                                        Console.ForegroundColor = ConsoleColor.Cyan;
-
-                                        Console.WriteLine("Reply from: " + rep.Address + "Bytes=" + rep.Buffer.Length + " Time=" +
-                                            rep.RoundtripTime + " TTL=" + rep.Options.Ttl + " Routers=" + (128 - rep.Options.Ttl) + " Status=" +
-                                            rep.Status + " Server" + item);
-                                        Thread.Sleep(1000);
-
-                                    }
-
-                                    check = false;
-
-
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Red;
-
-                                    hashmap[item.Key] = item.Value + 1;
-                                    Console.WriteLine("Error:{0}, {1}", item.Key, hashmap[item.Key]);
-                                    if (hashmap[item.Key] == 2)
-                                    {
-
-                                        Console.WriteLine("Error at server: {0}",  item.Key);
-                                        
-
-
-                                        string logFolderName = "logs/";
-                                        if (!Directory.Exists(logFolderName))
-                                        {
-                                            Directory.CreateDirectory(logFolderName);
-                                        }
-                                        string logFileName = "";
-                                        DateTime now = DateTime.Now;
-                                        logFileName = String.Format("{0}_{1}_{2}_log.txt", now.Day, now.Month, now.Year);
-                                        string fullFileLog = Path.Combine(logFolderName, logFileName);
-
-                                        using (StreamWriter sw = new StreamWriter(fullFileLog))
-                                        {
-                                            sw.WriteLine(String.Format("Error occurs at: {0}", now));
-                                            sw.WriteLine(String.Format("Error: {0}", ex.Message));
-                                        }
-
-                                    }
-
-
-
-                                    Thread.Sleep(1500);
-
-
-                                    check = false;
-
-
-                                }
-                            }
-                            else
-                            {
-                                check = false;
-                            }
-                        }
-
-                        check = true;
-                    }
-
-                }
-
+                //Or your Smtp Email ID and Password
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
             }
-
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);   
+            }
         }
     }
 }
